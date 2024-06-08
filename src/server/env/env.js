5e0d8de7f7155ba290559b84d71a4006ae49e125
Hania111 <chococookimonster@gmail.com>
2024-06-08 15:19:11 +0100

    init dla nowego remote

--- src/server/env/env.js
+++ src/server/env/env.js
@@ -1,6 +1,6 @@
 const cosmosPort = 10255; // replace with your port
 const dbName = 'my-recipes-db';
-const key = "";
+const key = "p7RmdfE1xmoZiYbOnTzwCTWQGw8N8Bnl2mBmzlcH0KYK2quIyMFuQPD7sHX2mHmlCeLxAN6mXkY8ACDbrizwUw==";
 
 module.exports = {
   dbName,
