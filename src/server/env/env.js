8300f07ec02117e5fe9ae035c14e14bdb5642c05
Hania111 <chococookimonster@gmail.com>
2024-06-09 16:53:35 +0100

    deleted key

--- src/server/env/env.js
+++ src/server/env/env.js
@@ -1,6 +1,6 @@
 const cosmosPort = 10255; // replace with your port
 const dbName = 'my-recipes-db';
-const key = "p7RmdfE1xmoZiYbOnTzwCTWQGw8N8Bnl2mBmzlcH0KYK2quIyMFuQPD7sHX2mHmlCeLxAN6mXkY8ACDbrizwUw==";
+const key = "";
 
 module.exports = {
   dbName,
